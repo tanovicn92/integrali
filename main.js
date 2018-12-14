e71713ab9ec010f8200da1530da88cb0726f4bcf
tanovicn92 <45176603+tanovicn92@users.noreply.github.com>
2018-12-14 20:00:14 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -20,3 +20,47 @@ function prikaziIntegrale() {
 	integrali.innerText="Prikaži integrale";
   }
 }
+function prikaziDiferencijalne() {
+  let t = document.getElementById("DiferencijalneDIV");
+  let diferencijalne = document.getElementById("diferencijalne");
+  if (t.style.display === "none") {
+    t.style.display = "block";
+	diferencijalne.innerText="Sakrij  diferencijalne jednačine";
+  } else {
+    t.style.display = "none";
+	diferencijalne.innerText="Prikaži diferencijalne jednačine";
+  }
+}
+function prikazi() {
+  let y = document.getElementById("DIV");
+  let integrali = document.getElementById("");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+	integrali.innerText="Sakrij ";
+  } else {
+    y.style.display = "none";
+	integrali.innerText="Prikaži ";
+  }
+}
+function prikazi() {
+  let y = document.getElementById("DIV");
+  let integrali = document.getElementById("");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+	integrali.innerText="Sakrij ";
+  } else {
+    y.style.display = "none";
+	integrali.innerText="Prikaži ";
+  }
+}
+function prikazi() {
+  let y = document.getElementById("DIV");
+  let integrali = document.getElementById("");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+	integrali.innerText="Sakrij ";
+  } else {
+    y.style.display = "none";
+	integrali.innerText="Prikaži ";
+  }
+}

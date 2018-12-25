c27b8049d447998fef7c6a31372c5c315be94504
tanovicn92 <45176603+tanovicn92@users.noreply.github.com>
2018-12-25 19:07:51 +0100

    Add files via upload

--- diferencijalne.js
+++ diferencijalne.js
@@ -191,6 +191,87 @@ function prikaziLinearneResenja3() {
 }
 
 
+function prikaziLinearne2r() {
+  let x = document.getElementById("Linearne2rDIV");
+  let Linearne2r = document.getElementById("linearne2r");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+	Linearne2r.innerText="Prikaži linearne d.j. drugog reda";
+  } else {
+    x.style.display = "block";
+	document.getElementById("linearne2r").innerText="Sakrij linearne d.j. drugog reda";
+  }
+}
+
+function prikaziLinearne2r1() {
+  let x = document.getElementById("Linearne2r1DIV");
+  let Linearne2r1 = document.getElementById("linearne2r1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+	Linearne2r1.innerText="Prikaži prvi zadatak";
+  } else {
+    x.style.display = "block";
+	document.getElementById("linearne2r1").innerText="Sakrij prvi zadatak";
+  }
+}
+function prikaziLinearne2r2() {
+  let x = document.getElementById("Linearne2r2DIV");
+  let Linearne2r2 = document.getElementById("linearne2r2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+	Linearne2r2.innerText="Prikaži drugi zadatak";
+  } else {
+    x.style.display = "block";
+	document.getElementById("linearne2r2").innerText="Sakrij drugi zadatak";
+  }
+}
+function prikaziLinearneResenja2r1() {
+  let x = document.getElementById("LinearneResenja2r1DIV");
+  let LinearneResenja2r1 = document.getElementById("linearneResenja2r1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+	LinearneResenja2r1.innerText="Prikaži rešenje prvog zadatka";
+  } else {
+    x.style.display = "block";
+	document.getElementById("linearneResenja2r1").innerText="Sakrij rešenje prvog zadatka";
+  }
+}
+function prikaziLinearneResenja2r2() {
+  let x = document.getElementById("LinearneResenja2r2DIV");
+  let LinearneResenja2r2 = document.getElementById("linearneResenja2r2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+	LinearneResenja2r2.innerText="Prikaži rešenje drugog zadatka";
+  } else {
+    x.style.display = "block";
+	document.getElementById("linearneResenja2r2").innerText="Sakrij rešenje drugog zadatka";
+  }
+}
+function prikaziLinearne2r3() {
+  let x = document.getElementById("Linearne2r3DIV");
+  let Linearne2r3 = document.getElementById("linearne2r3");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+	Linearne2r3.innerText="Prikaži treći zadatak";
+  } else {
+    x.style.display = "block";
+	document.getElementById("linearne2r3").innerText="Sakrij treći zadatak";
+  }
+}
+
+function prikaziLinearneResenja2r3() {
+  let x = document.getElementById("LinearneResenja2r3DIV");
+  let LinearneResenja2r3 = document.getElementById("linearneResenja2r3");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+	LinearneResenja2r3.innerText="Prikaži rešenje trećeg zadatka";
+  } else {
+    x.style.display = "block";
+	document.getElementById("linearneResenja2r3").innerText="Sakrij rešenje trećeg zadatka";
+  }
+}
+
+
 
 function prikaziDiferencijalne() {
   let t = document.getElementById("DiferencijalneDIV");
